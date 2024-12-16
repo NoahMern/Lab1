f6e74c054aa77dfb96aa88a9eef34fd608cd06cc
noahu <noahu@ABITY-2025>
2024-12-16 20:29:53 +0300

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,65 @@
+﻿using Lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+
+        stopwatch.OnStarted += (message) => Console.WriteLine(message);
+        stopwatch.OnStopped += (message) => Console.WriteLine(message);
+        stopwatch.OnReset += (message) => Console.WriteLine(message);
+
+        Console.WriteLine("Welcome to the Console Stopwatch!");
+        Console.WriteLine("Instructions:");
+        Console.WriteLine("Press 'S' to start the stopwatch.");
+        Console.WriteLine("Press 'T' to stop the stopwatch.");
+        Console.WriteLine("Press 'R' to reset the stopwatch.");
+        Console.WriteLine("Press 'Q' to quit the application.");
+
+        // We will run a loop to simulate the stopwatch ticking
+        bool running = true;
+        while (running)
+        {
+            // Check if there is user input
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(intercept: true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.S:
+                        stopwatch.Start();
+                        break;
+                    case ConsoleKey.T:
+                        stopwatch.Stop();
+                        break;
+                    case ConsoleKey.R:
+                        stopwatch.Reset();
+                        break;
+                    case ConsoleKey.Q:
+                        running = false;
+                        break;
+                }
+            }
+
+            if (stopwatch.IsRunning)
+            {
+                stopwatch.Tick();
+            }
+
+            ClearLineAndPrint(stopwatch.TimeElapsed);
+
+            Thread.Sleep(1000);
+        }
+
+       
+
+        Console.WriteLine("\nExiting the application...");
+    }
+
+    private static void ClearLineAndPrint(int timeElapsed)
+    {
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Write($"Elapsed Time: {timeElapsed} seconds   ");
+    }
+}
---
+++ Lab1/Stopwatch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Stopwatch
+    {
+        public delegate void StopwatchEventHandler(string message);
+
+        public event StopwatchEventHandler OnStarted;
+        public event StopwatchEventHandler OnStopped;
+        public event StopwatchEventHandler OnReset;
+
+        private int _timeElapsed;
+        public bool IsRunning { get; private set; }
+
+        public int TimeElapsed
+        {
+            get { return _timeElapsed; }
+        }
+
+        public void Start()
+        {
+            if (!IsRunning)
+            {
+                IsRunning = true;
+                OnStarted?.Invoke("Stopwatch Started!");
+            }
+        }
+
+        public void Stop()
+        {
+            if (IsRunning)
+            {
+                IsRunning = false;
+                OnStopped?.Invoke("Stopwatch Stopped!");
+            }
+        }
+
+        public void Reset()
+        {
+            IsRunning = false;
+            _timeElapsed = 0;
+            OnReset?.Invoke("Stopwatch Reset!");
+        }
+
+        public void Tick()
+        {
+            if (IsRunning)
+            {
+                _timeElapsed++;
+            }
+        }
+    }
+}
